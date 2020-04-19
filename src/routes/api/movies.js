4b7cab3de9aab2a865d165243c4f466163b5b13f
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2020-04-19 12:04:44 -0400

    add patch, delete, byid

--- src/routes/api/movies.js
+++ src/routes/api/movies.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../database/index");
 
-
 router.get("/", function (req, res) {
   db.select()
     .from("movies")
+    .orderBy("id")
     .then(function (data) {
       res.send(data);
     });
@@ -13,8 +13,36 @@ router.get("/", function (req, res) {
 
 router.post("/", function (req, res) {
   db.insert(req.body)
-    .returning('*')
-    .into('movies')
+    .returning("*")
+    .into("movies")
+    .then(function (data) {
+      res.send(data);
+    });
+});
+
+router.patch("/:id", function (req, res) {
+  db("movies")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .returning("*")
+    .then(function (data) {
+      res.send(data);
+    });
+});
+
+router.delete("/:id", function (req, res) {
+  db("movies")
+    .where({ id: req.params.id })
+    .del()
+    .then(function () {
+      res.json({ success: true });
+    });
+});
+
+router.get("/:id", function (req, res) {
+  db("movies")
+    .where({ id: req.params.id })
+    .select()
     .then(function (data) {
       res.send(data);
     });
