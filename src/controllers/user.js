5f0b63b5d8887af287c08eb52685cc4ba2668a98
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2020-04-23 19:58:43 -0400

    username unique

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -6,20 +6,25 @@ import * as Response from "../helpers/response/response";
 
 class UserData {
   static async addUser(req, res) {
-    const { password } = req.body;
+    const { username, password } = req.body;
     try {
       const result = await validator.validateAsync(req.body);
       if (!result.error) {
-        const hash = await bcrypt.hashPassword(password, 10);
-        const user = { ...req.body, password: hash };
-        const newUser = await db("user")
-          .returning(["id", "username", "password"])
-          .insert(user);
-        if (newUser.length > 0) {
-          const { id, username } = newUser[0];
-          const token = Token.sign({ username, userId: id });
-          const userData = { username, token };
-          return Response.responseOkUserCreated(res, userData);
+        const user = await db("user").where({ username: username });
+        if (user[0] != null) {
+          return Response.responseConflict(res, user);
+        } else {
+          const hash = await bcrypt.hashPassword(password, 10);
+          const user = { ...req.body, password: hash };
+          const newUser = await db("user")
+            .returning(["id", "username", "password"])
+            .insert(user);
+          if (newUser.length > 0) {
+            const { id, username } = newUser[0];
+            const token = Token.sign({ username, userId: id });
+            const userData = { username, token };
+            return Response.responseOkUserCreated(res, userData);
+          }
         }
       }
     } catch (error) {
